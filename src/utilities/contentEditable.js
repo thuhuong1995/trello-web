258e52718966ea41e24524ab8855f28604a8a388
GhostFury <thuhuongvnhubt@gmail.com>
2021-08-11 21:19:39 +0700

    fix-fail

---
+++ src/utilities/contentEditable.js
@@ -0,0 +1,9 @@
+export const selectAlltext = (e) => {
+    e.target.focus()
+    e.target.select()
+}
+export const saveContentEnterPress = (e) => {
+    if (e.keyCode === 13) {
+        e.target.blur()
+    }
+}
